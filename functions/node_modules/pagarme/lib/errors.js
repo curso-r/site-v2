df4c312ff8ea6b20a8fa68c03a976ee8db4c9b2e
Daniel Falbel <dfalbel@gmail.com>
2018-09-06 15:49:29 -0300

    mudando pastas

--- functions/node_modules/pagarme/lib/errors.js
+++
@@ -1,9 +0,0 @@
-class ApiError extends Error {
-  constructor (response) {
-    super('Pagar.me API error. Check error\'s "response" property for more details.')
-    this.name = 'ApiError'
-    this.response = response
-  }
-}
-
-export default ApiError
--- functions/node_modules/pagarme/lib/validations/validate/email.js
+++
@@ -1,5 +0,0 @@
-import { test } from 'ramda'
-
-const validate = test(/^([a-zA-Z0-9_\.\-\+])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)
-
-export default validate
