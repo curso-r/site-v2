ab7e620525508bf6a9fb1d432627e0c842180029
Daniel Falbel <dfalbel@gmail.com>
2018-10-02 15:50:32 -0300

    tentando arrumar

--- functions/pagarme.js
+++ functions/pagarme.js
@@ -73,15 +73,19 @@
 exports.handler = function (event, context, callback) {
   var pagarme = __webpack_require__(1);
 
-  console.log(event.queryStringParameters.token); //pagarme.client.connect({ api_key: process.env.PAGARME_API_KEY })
+  var token = event.queryStringParameters.token;
+  console.log('Received token: ', token); //pagarme.client.connect({ api_key: process.env.PAGARME_API_KEY })
   //  .then(client => client.transactions.capture({ id: event.data.TOKEN, amount: 1000 }));
 
   pagarme.client.connect({
     api_key: 'ak_test_ntiFjO9hdZo6K1IcZYRhJ8hyIrpPXH'
   }).then(client => client.transactions.capture({
-    id: event.data.TOKEN,
+    id: token,
     amount: 1000
-  }));
+  })); //.then(response => console.log(response))
+  //.catch(error => console.error(error));
+
+  console.log(response);
   callback(null, {
     statusCode: 200,
     body: "Hello, World"
--- src/pagarme.js
+++ src/pagarme.js
@@ -1,13 +1,19 @@
 exports.handler = function(event, context, callback) {
     var pagarme = require('pagarme');
     
-    console.log(event.queryStringParameters.token);
+    var token = event.queryStringParameters.token;
+    console.log('Received token: ', token);
+    
 
     //pagarme.client.connect({ api_key: process.env.PAGARME_API_KEY })
     //  .then(client => client.transactions.capture({ id: event.data.TOKEN, amount: 1000 }));
       
     pagarme.client.connect({ api_key: 'ak_test_ntiFjO9hdZo6K1IcZYRhJ8hyIrpPXH' })
-      .then(client => client.transactions.capture({ id: event.data.TOKEN, amount: 1000 }));
+      .then(client => client.transactions.capture({ id: token, amount: 1000 }));
+      //.then(response => console.log(response))
+      //.catch(error => console.error(error));
+    
+    console.log(response);
     
     callback(null, {
     statusCode: 200,
